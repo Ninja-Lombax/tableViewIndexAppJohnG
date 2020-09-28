2c030efbbc67a01c4eef6628d9e216786df70bf2
Ninja-Lombax <godzillakotm80@gmail.com>
2020-09-28 13:47:35 -0500

    Initial Commit

---
+++ tableViewIndexAppJohnG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tableViewIndexAppJohnG
+//
+//  Created by John Grasser on 9/28/20.
+//  Copyright © 2020 John Grasser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
